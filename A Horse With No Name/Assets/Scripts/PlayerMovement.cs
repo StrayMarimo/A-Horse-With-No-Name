8b83e138c45dad8a98d1ac6b584e0c03f5acefaf
StrayMarimo <anbasbas@gbox.adnu.edu.ph>
2023-10-08 23:05:05 +0800

    refactor: player movement

--- A Horse With No Name/Assets/Scripts/PlayerMovement.cs
+++ A Horse With No Name/Assets/Scripts/PlayerMovement.cs
@@ -7,301 +7,101 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     private GameObject KeySprite;
-    public HingeJoint2D FLA;
-    public HingeJoint2D FRA;
-    public HingeJoint2D BLA;
-    public HingeJoint2D BRA;
-    public HingeJoint2D FLB;
-    public HingeJoint2D FRB;
-    public HingeJoint2D BLB;
-    public HingeJoint2D BRB;
-    public HingeJoint2D FLC;
-    public HingeJoint2D FRC;
-    public HingeJoint2D BLC;
-    public HingeJoint2D BRC;
-
-    private JointMotor2D FLAMotorRef;
-    private JointMotor2D FRAMotorRef;
-    private JointMotor2D BLAMotorRef;
-    private JointMotor2D BRAMotorRef;
-    private JointMotor2D FLBMotorRef;
-    private JointMotor2D FRBMotorRef;
-    private JointMotor2D BLBMotorRef;
-    private JointMotor2D BRBMotorRef;
-    private JointMotor2D FLCMotorRef;
-    private JointMotor2D FRCMotorRef;
-    private JointMotor2D BLCMotorRef;
-    private JointMotor2D BRCMotorRef;
+    public HingeJoint2D[] legs = new HingeJoint2D[12];
+    private JointMotor2D[] motors = new JointMotor2D[12];
+    private Image[] keys = new Image[4];
     
-    public float hingeSpeed = 40;
-    public float hingeSpeed2 = 20;
-    public float hingeSpeed3 = 5;
+    public float hingeSpeed = 400;
+    public float hingeSpeed2 = 250;
+    public float hingeSpeed3 = 100;
     public Transform body;
     public Transform finishLine;
     public bool isDead = false;
+    private KeyCode[] keyCodes = {KeyCode.Q, KeyCode.W, KeyCode.O, KeyCode.P};
 
+    /// <summary>
+    /// Initializes the motors and keys arrays.
+    /// </summary>
     void Start()
     {
-        
-        FLAMotorRef = FLA.motor;
-        FRAMotorRef = FRA.motor;
-        BLAMotorRef = BLA.motor;
-        BRAMotorRef = BRA.motor;
-        FLBMotorRef = FLB.motor;
-        FRBMotorRef = FRB.motor;
-        BLBMotorRef = BLB.motor;
-        BRBMotorRef = BRB.motor;
-        FLCMotorRef = FLC.motor;
-        FRCMotorRef = FRC.motor;
-        BLCMotorRef = BLC.motor;
-        BRCMotorRef = BRC.motor;
+        for (int i = 0; i < 12; i++)
+        {
+            motors[i] = legs[i].motor;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            keys[i] = GameObject.FindGameObjectWithTag(keyCodes[i].ToString()).GetComponent<Image>();
+        }
     }
 
-    
+    /// <summary>
+    /// Updates the player's movement based on input from the user.
+    /// </summary>
     void Update()
     {
-        if (isDead) return;
-        if(Input.GetKey(KeyCode.Q))
+        if (isDead)
         {
-            onKeyPress("Q");
-            FLA.useMotor = true;
-            BRA.useMotor = true;
-            FRA.useMotor = true;
-            BLA.useMotor = true;
-            
-            FLB.useMotor = true;
-            BRB.useMotor = true;
-            FRB.useMotor = true;
-            BLB.useMotor = true;
-
-            FLC.useMotor = true;
-            BRC.useMotor = true;
-            FRC.useMotor = true;
-            BLC.useMotor = true;
-
-            FLAMotorRef.motorSpeed = -hingeSpeed;
-            BRAMotorRef.motorSpeed = -hingeSpeed;
-            FRAMotorRef.motorSpeed = hingeSpeed3;
-            BLAMotorRef.motorSpeed = hingeSpeed3;
-
-            FLBMotorRef.motorSpeed = -hingeSpeed2;
-            BRBMotorRef.motorSpeed = -hingeSpeed2;
-            FRBMotorRef.motorSpeed = hingeSpeed3;
-            BLBMotorRef.motorSpeed = hingeSpeed3;
-
-            FLCMotorRef.motorSpeed = -hingeSpeed3;
-            BRCMotorRef.motorSpeed = -hingeSpeed3;
-            FRCMotorRef.motorSpeed = hingeSpeed3;
-            BLCMotorRef.motorSpeed = hingeSpeed3;
-
-            FLA.motor = FLAMotorRef;
-            BRA.motor = BRAMotorRef;
-            FRA.motor = FRAMotorRef;
-            BLA.motor = BLAMotorRef;
-
-            FLB.motor = FLBMotorRef;
-            BRB.motor = BRBMotorRef;
-            FRB.motor = FRBMotorRef;
-            BLB.motor = BLBMotorRef;
-
-            FLC.motor = FLCMotorRef;
-            BRC.motor = BRCMotorRef;
-            FRC.motor = FRCMotorRef;
-            BLC.motor = BLCMotorRef;
+            onKeyRelease();
+            return;
         }
-
-        else if(Input.GetKey(KeyCode.W))
+        for (int i = 0; i <= 3; i++)
         {
-            onKeyPress("W");
-            FLB.useMotor = true;
-            BRB.useMotor = true;
-            FRB.useMotor = true;
-            BLB.useMotor = true;
-
-            FLC.useMotor = true;
-            BRC.useMotor = true;
-            FRC.useMotor = true;
-            BLC.useMotor = true;
-
-            FLBMotorRef.motorSpeed = -hingeSpeed;
-            BRBMotorRef.motorSpeed = -hingeSpeed;
-            FRBMotorRef.motorSpeed = hingeSpeed3;
-            BLBMotorRef.motorSpeed = hingeSpeed3;
-
-            FLCMotorRef.motorSpeed = -hingeSpeed2;
-            BRCMotorRef.motorSpeed = -hingeSpeed2;
-            FRCMotorRef.motorSpeed = hingeSpeed3;
-            BLCMotorRef.motorSpeed = hingeSpeed3;
-
-            FLB.motor = FLBMotorRef;
-            BRB.motor = BRBMotorRef;
-            FRB.motor = FRBMotorRef;
-            BLB.motor = BLBMotorRef;
-
-            FLC.motor = FLCMotorRef;
-            BRC.motor = BRCMotorRef;
-            FRC.motor = FRCMotorRef;
-            BLC.motor = BLCMotorRef;
-            
+            if (Input.GetKeyDown(keyCodes[i]))
+            {
+                keys[i].color = Color.red;
+                setMotorSpeedPair(i * 3);
+            }
         }
-        else if(Input.GetKey(KeyCode.E))
+        for (int i = 0; i <=3; i++)
         {
-            onKeyPress("E");
-            FLC.useMotor = true;
-            BRC.useMotor = true;
-            FRC.useMotor = true;
-            BLC.useMotor = true;
-
-            FLCMotorRef.motorSpeed = -hingeSpeed;
-            BRCMotorRef.motorSpeed = -hingeSpeed;
-            FRCMotorRef.motorSpeed = hingeSpeed3;
-            BLCMotorRef.motorSpeed = hingeSpeed3;
-
-            FLC.motor = FLCMotorRef;
-            BRC.motor = BRCMotorRef;
-            FRC.motor = FRCMotorRef;
-            BLC.motor = BLCMotorRef;
-            
+            if (Input.GetKeyUp(keyCodes[i]))
+            {
+                onKeyRelease(i);
+            }
         }
-
-        else if(Input.GetKey(KeyCode.I))
-        {
-            onKeyPress("I");
-            FRA.useMotor = true;
-            BLA.useMotor = true;
-            FLA.useMotor = true;
-            BRA.useMotor = true;
-
-            FRB.useMotor = true;
-            BLB.useMotor = true;
-            FLB.useMotor = true;
-            BRB.useMotor = true;
-
-            FRC.useMotor = true;
-            BLC.useMotor = true;
-            FLC.useMotor = true;
-            BRC.useMotor = true;
-
-            FRAMotorRef.motorSpeed = -hingeSpeed;
-            BLAMotorRef.motorSpeed = -hingeSpeed;
-            FLAMotorRef.motorSpeed = hingeSpeed3;
-            BRAMotorRef.motorSpeed = hingeSpeed3;
-
-            FRBMotorRef.motorSpeed = -hingeSpeed2;
-            BLBMotorRef.motorSpeed = -hingeSpeed2;
-            FLBMotorRef.motorSpeed = hingeSpeed3;
-            BRBMotorRef.motorSpeed = hingeSpeed3;
-
-            FRCMotorRef.motorSpeed = -hingeSpeed3;
-            BLCMotorRef.motorSpeed = -hingeSpeed3;
-            FLCMotorRef.motorSpeed = hingeSpeed3;
-            BRCMotorRef.motorSpeed = hingeSpeed3;
-
-
-            FRA.motor = FRAMotorRef;
-            BLA.motor = BLAMotorRef;
-            FLA.motor = FLAMotorRef;
-            BRA.motor = BRAMotorRef;
-
-            FRB.motor = FRBMotorRef;
-            BLB.motor = BLBMotorRef;
-            FLB.motor = FLBMotorRef;
-            BRB.motor = BRBMotorRef;
-
-            FRC.motor = FRCMotorRef;
-            BLC.motor = BLCMotorRef;
-            FLC.motor = FLCMotorRef;
-            BRC.motor = BRCMotorRef;
-            
-        }
-        else if(Input.GetKey(KeyCode.O))
-        {
-            onKeyPress("O");
-            FRB.useMotor = true;
-            BLB.useMotor = true;
-            FLB.useMotor = true;
-            BRB.useMotor = true;
-
-            FRC.useMotor = true;
-            BLC.useMotor = true;
-            FLC.useMotor = true;
-            BRC.useMotor = true;
-
-            FRBMotorRef.motorSpeed = -hingeSpeed;
-            BLBMotorRef.motorSpeed = -hingeSpeed;
-            FLBMotorRef.motorSpeed = hingeSpeed3;
-            BRBMotorRef.motorSpeed = hingeSpeed3;
-
-            FRCMotorRef.motorSpeed = -hingeSpeed2;
-            BLCMotorRef.motorSpeed = -hingeSpeed2;
-            FLCMotorRef.motorSpeed = hingeSpeed3;
-            BRCMotorRef.motorSpeed = hingeSpeed3;
-
-            FRB.motor = FRBMotorRef;
-            BLB.motor = BLBMotorRef;
-            FLB.motor = FLBMotorRef;
-            BRB.motor = BRBMotorRef;
-
-            FRC.motor = FRCMotorRef;
-            BLC.motor = BLCMotorRef;
-            FLC.motor = FLCMotorRef;
-            BRC.motor = BRCMotorRef;
-            
-        }
-
-        else if(Input.GetKey(KeyCode.P))
-        {
-            onKeyPress("P");
-            FRC.useMotor = true;
-            BLC.useMotor = true;
-            FLC.useMotor = true;
-            BRC.useMotor = true;
-
-            FRCMotorRef.motorSpeed = -hingeSpeed;
-            BLCMotorRef.motorSpeed = -hingeSpeed;
-            FLCMotorRef.motorSpeed = hingeSpeed3;
-            BRCMotorRef.motorSpeed = hingeSpeed3;
-
-            FRC.motor = FRCMotorRef;
-            BLC.motor = BLCMotorRef;
-            FLC.motor = FLCMotorRef;
-            BRC.motor = BRCMotorRef;
-            
-        }
-
-        else {
-        
-            FLA.useMotor = false;
-            FRA.useMotor = false;
-            BLA.useMotor = false;
-            BRA.useMotor = false;
-            FLB.useMotor = false;
-            FRB.useMotor = false;
-            BLB.useMotor = false;
-            BRB.useMotor = false;
-            FLC.useMotor = false;
-            FRC.useMotor = false;
-            BLC.useMotor = false;
-            BRC.useMotor = false;
-            onKeyRelease("Q");
-            onKeyRelease("W");
-            onKeyRelease("E");
-            onKeyRelease("I");
-            onKeyRelease("O");
-            onKeyRelease("P");
-
-        }
-        
     }
 
-    void onKeyPress(string key){
-        KeySprite = GameObject.FindGameObjectWithTag(key);
-        KeySprite.GetComponent<Image>().color = Color.red;
+    /// <summary>
+    /// Sets the motor speed for a a leg with 3 parts
+    /// </summary>
+    /// <param name="index">The index of the first leg in the group.</param>
+    private void setMotorSpeedPair(int index){
+        legs[index].useMotor = true;
+        legs[index + 1].useMotor = true;
+        legs[index + 2].useMotor = true;
+        motors[index].motorSpeed = -hingeSpeed;
+        motors[index + 1].motorSpeed = -hingeSpeed2;
+        motors[index + 2].motorSpeed = -hingeSpeed3;
+        legs[index].motor = motors[index];
+        legs[index + 1].motor = motors[index + 1];
+        legs[index + 2].motor = motors[index + 2];
     }
 
-    void onKeyRelease(string key){
-        KeySprite = GameObject.FindGameObjectWithTag(key);
-        KeySprite.GetComponent<Image>().color = Color.white;
+
+
+    /// <summary>
+    /// Changes the color of the key at the specified index to white 
+    /// and stops the corresponding leg parts' motors.
+    /// </summary>
+    /// <param name="index">The index of the key to release.</param>
+    private void onKeyRelease(int index){
+        keys[index].color = Color.white;
+        for(int i = index * 3; i < index * 3 + 3; i++)
+        {
+            legs[i].useMotor = false;
+        }
     }
 
+    /// <summary>
+    /// Handles the release of all keys.
+    /// </summary>
+    private void onKeyRelease(){
+        for (int i = 0; i <=3 ; i++){
+            keys[i].color = Color.white;
+        }
+        for(int i = 0; i < 12; i++)
+        {
+            legs[i].useMotor = false;
+        }
+    }
 }
