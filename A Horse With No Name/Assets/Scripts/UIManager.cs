a97894aba3741a6a620653a170290e2545421049
StrayMarimo <anbasbas@gbox.adnu.edu.ph>
2023-10-09 14:01:37 +0800

    docs: ui manager

--- A Horse With No Name/Assets/Scripts/UIManager.cs
+++ A Horse With No Name/Assets/Scripts/UIManager.cs
@@ -1,30 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+// This script is used to manage the UI buttons in the game
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Threading.Tasks;
-using UnityEngine.EventSystems;
 public class UIManager : MonoBehaviour
 {
     private AudioManager audioManager;
+
+    // called when the game starts
     void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
+
+    /// <summary>
+    /// Called when the player goes back from leaderboard
+    /// </summary>
     public void GoToGame()
     {
+        // unload leaderboard scene
         SceneManager.UnloadSceneAsync("Leaderboard");
+
+        // resume game
         audioManager.PlayClickSFX();
         audioManager.ResumeBGM();
         Time.timeScale = 1;
     }
     
+    /// <summary>
+    /// Called when the player goes to leaderboard
+    /// </summary>
     public void GoToLeaderboards ()
     {
+        // pause game
         audioManager.PlayClickSFX();
         audioManager.PauseBGM();
         Time.timeScale = 0;
-        GameObject.FindGameObjectWithTag("Click").GetComponent<AudioSource>().Play();
+
+        // load leaderboard scene
         SceneManager.LoadSceneAsync("Leaderboard", LoadSceneMode.Additive);
     }
 }
